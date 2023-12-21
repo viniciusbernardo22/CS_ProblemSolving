6b9bda3739ab0c10c3270bad2b8b7609bb9cb4ad
Vinicius <vini383@gmail.com>
2023-12-21 15:14:29 -0300

    feat: added printService

---
+++ printService/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace printService;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Clear();
+        PrintService<int> printService = new ();
+
+        Console.Write("How many values? ");
+
+        int n = int.Parse(Console.ReadLine());
+       
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write($"value {i + 1} is: ");
+            int x = int.Parse(Console.ReadLine());
+            printService.AddValue(x);
+
+        }
+
+        printService.Print();
+
+        Console.WriteLine($" The first one is: {printService.First()}");
+
+    }
+}
---
+++ printService/README.md
@@ -0,0 +1,11 @@
+Solução apenas para inteiros:
+
+![problem1](https://github.com/viniciusbernardo22/PrintService_CS/assets/83471044/7c851d88-6837-43e7-9bf2-16148e4c75ca)
+
+Solução com generics: 
+
+![generics](https://github.com/viniciusbernardo22/PrintService_CS/assets/83471044/c5c0f413-ca69-46b2-8e77-1e5dac24681a)
+
+Output: 
+
+![problem1solved](https://github.com/viniciusbernardo22/PrintService_CS/assets/83471044/2a0df976-47b7-4c23-b05a-2350f8da6cda)
